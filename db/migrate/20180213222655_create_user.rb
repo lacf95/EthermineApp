595c7aa97f6394e68b045c185930b606908cd3cb
Isaias Orozco <isaororio@gmail.com>
2018-02-13 17:52:54 -0600

    fixing names errors

--- db/migrate/20180213222655_create_user.rb
+++ db/migrate/20180213222655_create_user.rb
@@ -1,9 +1,8 @@
 class CreateUser < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :user_name
+      t.string :first_name
       t.string :last_name
-      t.string :string
       t.string :email
       t.string :password
       t.timestamps
