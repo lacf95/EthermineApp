d93d7e230e319fc08cda1f93cfbfc318a44a362f
Pedro Terriquez <pedroterriquez@hotmail.com>
2018-02-14 12:09:24 -0600

    Registration complete

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,10 +1,9 @@
 class RegistrationController < ApplicationController
   def index
-    #@registration = User.new
+    @user = User.new
   end
 
   def create
-    binding.pry
     @user = User.new(user_params)
     if @user.save
       redirect_to home_index_path
@@ -14,6 +13,6 @@ class RegistrationController < ApplicationController
   private
 
   def user_params
-    params.require(:registration).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
