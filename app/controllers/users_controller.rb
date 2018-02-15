ef1aec0773805f4b5b80e3c40831adcdcb5264c2
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2018-02-15 11:43:55 -0600

    Sidebar with general info completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def index
     @user = User.find(session[:user_id])
-    @pool = EtherClient.new.pool
+    general_ethermine
+  end
+
+  def general_ethermine
+    ether = EtherClient.new
+    stats = ether.pool.statistics
+    @pool_stats = stats.pool_stats
+    @price = stats.price
+    @network = ether.pool.network
   end
 end
