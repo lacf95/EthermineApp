2f51851436a57f2a484c7dc762901522617df643
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2018-02-16 15:48:28 -0600

    Refactored token functionality

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,28 +5,24 @@ class RegistrationController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.token = token(@user.email)
+    @user.new_token
     return render 'index' unless @user.save
     session[:user_id] = @user.id
     redirect_to home_index_path
   end
 
   def confirm
-    @user = User.find_by(token: params[:token])
-    if @user
-      @user.active = true
-      return render 'error' unless @user.save
-      redirect_to home_index_path
-    else
-      render 'error'
-    end
+    user = User.find_by(token: params[:token])
+    return render 'error' unless user
+    user.active = true
+    return render 'error' unless user.save
+    redirect_to home_index_path
   end
 
   def signup_email
     user = User.find(session[:user_id])
-    user.token = token(user.email)
-    return render 'error' unless user.save
-    send_email
+    return render 'error' unless user.change_token
+    send_email(user.token)
     redirect_to home_index_path
   end
 
@@ -34,10 +30,6 @@ class RegistrationController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
-  end
-
-  def token(email)
-    Digest::SHA256.hexdigest(email + Time.now.to_i.to_s)
   end
 
   def send_email(token)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true, length: { maximum: 20 }, on: :create
   validates :last_name, presence: true, length: { maximum: 20 }, on: :create
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  def change_token
+    update(token: generate_token)
+  end
+
+  def new_token
+    write_attribute(:token, generate_token)
+  end
+
+  private
+
+  def generate_token
+    Digest::SHA256.hexdigest(email + Time.now.to_i.to_s)
+  end
 end
