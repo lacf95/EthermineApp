3f808574196649ec64eaf1eba0af637214cf6cc1
Isaias Orozco <isaororio@gmail.com>
2018-02-19 13:28:49 -0600

    Fixing name errors, now in the navbar can show the current user name or the user actual session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :show_user_name
+  def show_user_name
+    return nil unless a = User.find_by(id: session[:user_id])
+    a.first_name
+  end
+
 end
